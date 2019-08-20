c606a918ca865d4fa55851021877e288b187aaf9
celestin <arch.liu@foxmail.com>
2019-08-20 19:40:03 +0800

    wip

--- yuv2jpg_sort.sh
+++ yuv2jpg_sort.sh
@@ -15,7 +15,7 @@ fi
 cat CALL.rect | while read line
 do
     # echo $line
-    # CALL-1.yuv timestamp:1565754707 stage1: stage2:F
+    # CALL-1.yuv timestamp:1565754707 stage1:T stage2:F
     echo $((i++))
     if [[ $line =~ ([^ ]+)\ ([^ ]+)\ (stage1[^ ]+)(\ (stage2[^ ]+))* ]]; then
         yuv_file=${BASH_REMATCH[1]}
@@ -24,10 +24,11 @@ do
         stage2=${BASH_REMATCH[4]}
         if [ -f "$yuv_file" ] #file
         then
-            echo "ok"
-            if [[ $stage1 != '' && $stage2 == '' ]]; then
+            if [[ $stage1 =~ 'stage1:T' && $stage2 =~ 'stage2:F' ]]; then
+                echo "stage1"
                 ffmpeg -y -s 640x360 -i $yuv_file ./jpg1/${yuv_file%.*}.jpg < /dev/null
-            elif [[ $stage1 != '' && $stage2 != '' ]]; then
+            elif [[ $stage1 =~ 'stage1:T' && $stage2 =~ 'stage2:T' ]]; then
+                echo "stage1&stage2"
                 ffmpeg -y -s 640x360 -i $yuv_file ./jpg2/${yuv_file%.*}.jpg < /dev/null
             fi
             rm -f $yuv_file
