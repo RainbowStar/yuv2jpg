46ca98116538696ecc37d70c6a9918b0523494c8
celestin <arch.liu@foxmail.com>
2019-09-04 22:10:09 +0800

    wip

--- yuv2jpg_drawbox.sh
+++ yuv2jpg_drawbox.sh
@@ -11,9 +11,8 @@
 #call_stage2_version.rect：记录stage1:T&stage2:T(先写入文件再转换)
 #call_stage1_verison.rect&call_stage2_version.rect每个图片记录增加版本信息
 # CALL-16.yuv timestamp:1565754716 stage1:T-0.404722-upperbody[(188,70),(422,70),(422,332),(188,332)]-gesture[(326,184),(388,184),(388,240),(326,240)] stage2:T-1.349073-gestureex[(294,152),(418,152),(418,268),(294,268)]-gesture[(324,188),(386,188),(386,244),(324,244)]
-#版   本：V0.1
+#版   本：V0.2
 #修改记录：(从高到底排序)
-#V0.1
 # ffmpeg cmd must add '< /dev/null'
 
 version="1.0.0.17"
@@ -50,19 +49,20 @@ do
   fi
 done
 
-line_num=$(\wc -l < $list_file)
 
+#### start prcessing ####
+echo "======================="
+
+line_num=$(\wc -l < $list_file)
 i=1
+
 cat $list_file | while read line
 do
     # echo $line
     printf "line %d / %d : \t" $((i++)) $line_num
     #      CALL-n.yuv timestamp:nnn... stage1:T... stage2:F...
     if [[ $line =~ ([^ ]+)\ ([^ ]+)\ (stage1[^ ]+)(\ (stage2[^ ]+))* ]]; then
-        yuv_file=${BASH_REMATCH[1]}
-        timestamp=${BASH_REMATCH[2]}
-        stage1_b=${BASH_REMATCH[3]}
-        stage2_b=${BASH_REMATCH[4]}
+        yuv_file=${BASH_REMATCH[1]}; timestamp=${BASH_REMATCH[2]}; stage1_b=${BASH_REMATCH[3]}; stage2_b=${BASH_REMATCH[4]};
         if [ -f "$yuv_file" ] #file
         then
             if [[ $stage1_b =~ 'stage1:T' && $stage2_b =~ 'stage2:F' ]]; then
@@ -80,14 +80,18 @@ do
             else
                 echo ""
             fi
+
             # stage1:T-0.404722-upperbody[(188,70),(422,70),(422,332),(188,332)]-gesture[(326,184),(388,184),(388,240),(326,240)] stage2:T-1.349073-gestureex[(294,152),(418,152),(418,268),(294,268)]-gesture[(324,188),(386,188),(386,244),(324,244)]
             if [[ $stage1_b =~ stage1:T-.+-upperbody\[\(([0-9]+),([0-9]+)\),\(.+?\),\(([0-9]+),([0-9]+)\),\(.+?\)\]-gesture\[\(([0-9]+),([0-9]+)\),\(.+?\),\(([0-9]+),([0-9]+)\),\(.+?\)\] ]]; then
                 # echo ${BASH_REMATCH[1]} ${BASH_REMATCH[2]} ${BASH_REMATCH[3]} ${BASH_REMATCH[4]} 
                 body_llx=${BASH_REMATCH[1]}; body_lly=${BASH_REMATCH[2]}; body_urx=${BASH_REMATCH[3]}; body_ury=${BASH_REMATCH[4]};
                 gest_llx=${BASH_REMATCH[5]}; gest_lly=${BASH_REMATCH[6]}; gest_urx=${BASH_REMATCH[7]}; gest_ury=${BASH_REMATCH[8]};
                 \ffmpeg -loglevel $loglevel -y -s $res -i $yuv_file \
-                        -vf drawbox=x=$body_llx:y=$body_lly:w=$body_urx-$body_llx:h=$body_ury-$body_lly:t=2:color=green@0.8,drawbox=x=$gest_llx:y=$gest_lly:w=$gest_urx-$gest_llx:h=$gest_ury-$gest_lly:t=2:color=red@0.8 \
+                        -vf "drawbox=x=$body_llx:y=$body_lly:w=$body_urx-$body_llx:h=$body_ury-$body_lly:t=2:color=green@0.8,\
+                             drawbox=x=$gest_llx:y=$gest_lly:w=$gest_urx-$gest_llx:h=$gest_ury-$gest_lly:t=2:color=red@0.8" \
                         ./${jpg_dir}/${yuv_file%.*}_${version}.jpg < /dev/null
+            #else
+            #    echo "drawbox format error"
             fi
             \rm -f $yuv_file
         else
@@ -98,5 +102,6 @@ do
     fi
 done
 
-echo "finished!"
+echo "======================="
+echo "$0 finished!"
 
